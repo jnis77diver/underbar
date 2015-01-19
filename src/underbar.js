4a45d3143be1d36108fcf25efe7051472c578753
Jonah Nisenson <jsnisenson@gmail.com>
2015-01-18 19:42:40 -0500

    Fix _first, _last, _each functions.

--- src/underbar.js
+++ src/underbar.js
@@ -7,6 +7,7 @@
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
   _.identity = function(val) {
+     return val;
   };
 
   /**
@@ -37,6 +38,9 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    if( n === 0 ) return [];
+    if( n > array.length ) return array;
+    return n === undefined? array[array.length-1]: array.slice(array.length - n);
   };
 
   // Call iterator(value, key, collection) for each element of collection.
@@ -45,6 +49,16 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if( Array.isArray(collection) ) {
+      for( var i = 0, x = collection.length ; i < x; i++ ) {
+        iterator( collection[i], i, collection );
+      }
+    }
+    else if ( collection.constructor === Object ) {
+      for( var key in collection ) {
+        if( collection.hasOwnProperty(key) ) iterator( collection[key], key, collection); 
+    }
+    }
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
